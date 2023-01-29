ba67af76422e09f5f0b6e67d741b81b3f05f7346
EnriqueIzel2 <eleaoizel@gmail.com>
2023-01-28 21:19:12 -0400

    feat: create transaction to fragments

--- app/src/main/java/com/example/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/example/spotifyclone/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.spotifyclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.example.spotifyclone.Fragments.Biblioteca
+import com.example.spotifyclone.Fragments.Buscar
+import com.example.spotifyclone.Fragments.Home
+import com.example.spotifyclone.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+  private var content: FrameLayout? = null
+  private var mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    when (item.itemId) {
+      R.id.menu_item_inicio -> {
+        val fragment = Home.newInstance()
+        addFragment(fragment)
+      }
+
+      R.id.menu_item_buscar -> {
+        val fragment = Buscar.newInstance()
+        addFragment(fragment)
+      }
+
+      R.id.menu_item_biblioteca -> {
+        val fragment = Biblioteca.newInstance()
+        addFragment(fragment)
+      }
+    }
+    false
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    content = binding.mainContent
+    binding.bottomNavMenu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+  }
+
+  private fun addFragment(fragment: Fragment) {
+    supportFragmentManager.beginTransaction().replace(R.id.main_content, fragment, fragment.javaClass.simpleName)
+      .commit()
   }
 }
