795c1265ccb8575a36bfcc71b4bbbc281bc6e212
EnriqueIzel2 <eleaoizel@gmail.com>
2023-02-15 21:46:34 -0400

    feat: create data for Secao, the data is a mock

--- app/src/main/java/com/example/spotifyclone/model/Dados.kt
+++ app/src/main/java/com/example/spotifyclone/model/Dados.kt
@@ -1,5 +1,6 @@
 package com.example.spotifyclone.model
 
+import com.example.spotifyclone.R
 import com.google.gson.annotations.SerializedName
 
 data class Categoria(
@@ -14,4 +15,53 @@ data class Album(
 data class Categorias(
   @SerializedName("categoria")
   val categorias: List<Categoria>
+)
+
+data class Secao(
+  var secao: Int,
+  var nomeSecao: String
+)
+
+class SecaoBuilder {
+  var secao: Int = 0
+  var nomeSecao: String = ""
+
+  fun build(): Secao = Secao(secao, nomeSecao)
+}
+
+fun secao(block: SecaoBuilder.() -> Unit): Secao = SecaoBuilder().apply(block).build()
+
+fun dados(): MutableList<Secao> = mutableListOf(
+  secao {
+    secao = R.drawable.secao1
+    nomeSecao = "Podcasts"
+  },
+  secao {
+    secao = R.drawable.secao2
+    nomeSecao = "Lançamentos"
+  },
+  secao {
+    secao = R.drawable.secao3
+    nomeSecao = "Em casa"
+  },
+  secao {
+    secao = R.drawable.secao4
+    nomeSecao = "Paradas"
+  },
+  secao {
+    secao = R.drawable.secao1
+    nomeSecao = "Shows"
+  },
+  secao {
+    secao = R.drawable.secao2
+    nomeSecao = "Para Você"
+  },
+  secao {
+    secao = R.drawable.secao3
+    nomeSecao = "Malhação"
+  },
+  secao {
+    secao = R.drawable.secao4
+    nomeSecao = "MPB"
+  },
 )
