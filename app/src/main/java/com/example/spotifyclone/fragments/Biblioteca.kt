cdde0695edd7e39661802b7deefdedfe449ce67c
EnriqueIzel2 <eleaoizel@gmail.com>
2023-02-18 22:19:16 -0400

    feat: create adapter for viewPagerTab

--- app/src/main/java/com/example/spotifyclone/fragments/Biblioteca.kt
+++ app/src/main/java/com/example/spotifyclone/fragments/Biblioteca.kt
@@ -1,20 +1,39 @@
 package com.example.spotifyclone.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.spotifyclone.R
+import com.example.spotifyclone.databinding.FragmentBibliotecaBinding
+import com.example.spotifyclone.fragments.pagertab.Albuns
+import com.example.spotifyclone.fragments.pagertab.Artistas
+import com.example.spotifyclone.fragments.pagertab.Playlists
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 
-class Biblioteca : Fragment() {
+class Biblioteca : Fragment(R.layout.fragment_biblioteca) {
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_biblioteca, container, false)
+  private lateinit var binding: FragmentBibliotecaBinding
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    val bindingBiblioteca = FragmentBibliotecaBinding.bind(view)
+    binding = bindingBiblioteca
+
+    val viewPager = binding.viewpager
+    val viewPagerTab = binding.viewpagertab
+
+    super.onViewCreated(view, savedInstanceState)
+    var adapter = FragmentPagerItemAdapter(
+      fragmentManager, FragmentPagerItems.with(context)
+        .add("Playlists", Playlists::class.java)
+        .add("Artistas", Artistas::class.java)
+        .add("Álbuns", Albuns::class.java)
+        .create()
+    )
+
+    viewPager.adapter = adapter
+    viewPagerTab.setViewPager(viewPager)
   }
 
   companion object {
