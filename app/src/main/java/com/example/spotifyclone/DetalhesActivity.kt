12cf4ed7ba1b40c089e9a59e84911f9f4bfa24a0
EnriqueIzel2 <eleaoizel@gmail.com>
2023-02-28 22:00:58 -0400

    feat: create navigation for back for Home activity

--- app/src/main/java/com/example/spotifyclone/DetalhesActivity.kt
+++ app/src/main/java/com/example/spotifyclone/DetalhesActivity.kt
@@ -1,9 +1,11 @@
 package com.example.spotifyclone
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.spotifyclone.databinding.ActivityDetalhesBinding
+import com.example.spotifyclone.fragments.Home
 
 class DetalhesActivity : AppCompatActivity() {
 
@@ -14,7 +16,17 @@ class DetalhesActivity : AppCompatActivity() {
     binding = ActivityDetalhesBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    val toolbar = binding.toolbar
+
     window.statusBarColor = Color.LTGRAY
-    binding.toolbar.navigationIcon = getDrawable(R.drawable.ic_arrow_back)
+    toolbar.navigationIcon = getDrawable(R.drawable.ic_arrow_back)
+    toolbar.setNavigationOnClickListener {
+      val intent = Intent(this, Home::class.java)
+      startActivities(intent)
+      finish()
+    }
+  }
+
+  private fun startActivities(intent: Intent) {
   }
 }
